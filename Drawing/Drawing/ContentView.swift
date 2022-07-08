1b74799695e2f5a5329e11d7c01f009ba32e18ad
Alex Bardi <alex.bardi@hey.com>
2022-07-07 22:13:43 -0500

    Finished drawing prj

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,16 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var insetAmount = 50.0
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount = 1.0
+    @State private var hue = 0.6
+    
     
     var body: some View {
-        Trapezoid(insetAmount: insetAmount)
-            .frame(width: 200, height: 100)
-            .onTapGesture {
-                withAnimation {
-                    insetAmount = Double.random(in: 10...90)
-                }
+        VStack(spacing: 0) {
+            Spacer()
+            
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+                .frame(width: 300, height: 300)
+            
+            Spacer()
+            
+            Group {
+                Text("Inner radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Outer radius: \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Distance \(Int(distance))")
+                Slider(value: $distance, in: 1...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Amount \(amount, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $amount)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Color")
+                Slider(value: $hue)
+                    .padding([.horizontal])
+
             }
+        }
     }
 }
 
@@ -27,6 +57,92 @@ struct ContentView: View {
 
 
 /* Structs from pre-project practice */
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: Double
+    
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
+        let outerRadius = Double(self.outerRadius)
+        let innerRadius = Double(self.innerRadius)
+        let distance = Double(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * Double.pi * outerRadius / Double(divisor)) * amount
+        
+        var path = Path()
+        
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+            
+            x += rect.width / 2
+            y += rect.height / 2
+            
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+        return path
+    }
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+        
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+        
+        return a
+    }
+}
+
+
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // figure out how big each row/columns needs to be
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+        
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        return path
+    }
+}
 
 struct Trapezoid: Shape {
     var insetAmount: Double
