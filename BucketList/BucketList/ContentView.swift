bbb698c34ebf1afe876dc9e1d7b951bc4ec3709c
Alex Bardi <alex.bardi@hey.com>
2023-05-25 07:42:36 -0500

    using enums to switch views

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -7,30 +7,58 @@
 
 import SwiftUI
 
-struct ContentView: View {
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .onTapGesture {
-                    let str = "Test Message"
-                    let url = getDocumentsDirectory().appendingPathComponent("message.txt")
-                    
-                    do {
-                        try str.write(to: url, atomically: true, encoding: .utf8)
-                        
-                        let input = try String(contentsOf: url)
-                        print(input)
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-                }
-        }
-        .padding()
+        Text("Loading...")
     }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed :(")
+    }
+}
+
+struct ContentView: View {
+    @State var loadingState = LoadingState.loading
     
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
+    var body: some View {
+        
+        Spacer()
+        
+        Button("Toggle") {
+            switch loadingState {
+            case .loading:
+                loadingState = .success
+            case .success:
+                loadingState = .failed
+            case .failed:
+                loadingState = .loading
+            }
+        }
+        
+        Spacer()
+        
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
+        }
+        
+        Spacer()
     }
 }
 
