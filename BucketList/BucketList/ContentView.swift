41d993c54071b1ebb11eb22bb857c961949bc99e
Alex Bardi <alex.bardi@hey.com>
2023-05-27 00:00:18 -0500

    Day 69: biometrics

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -6,34 +6,39 @@
 //
 
 import SwiftUI
-import MapKit
-
-struct Location: Identifiable {
-    let id = UUID()
-    let name: String
-    let coordinate: CLLocationCoordinate2D
-}
+import LocalAuthentication
 
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var isUnlocked = false
     
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
     var body: some View {
-        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-            MapAnnotation(coordinate: location.coordinate) {
-                VStack {
-                    Circle()
-                        .stroke(.red, lineWidth: 3)
-                        .frame(width: 44, height: 44)
-                    Text(location.name)
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    // auth failed
                 }
+                
             }
         }
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
