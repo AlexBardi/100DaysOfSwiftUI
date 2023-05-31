09d7012e4de837c005158edfd6371a21772131c9
Alex Bardi <alex.bardi@hey.com>
2023-05-30 21:42:32 -0500

    Day 71: Getting cool stuff from wikipedia

---
+++ BucketList/BucketList/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  BucketList
+//
+//  Created by Alex Bardi on 5/30/23.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
