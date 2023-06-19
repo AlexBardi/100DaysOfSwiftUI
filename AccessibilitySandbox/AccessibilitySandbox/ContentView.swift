85c9dce7d1289e943864d80b8bee328dde4e487d
Alex Bardi <alex.bardi@hey.com>
2023-06-18 23:10:19 -0500

    Day 74: Learned about ignoring children

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -8,31 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-       "ales-krivec-15949",
-       "galina-n-189483",
-       "kevin-horstmann-141705",
-       "nicolas-tissot-335096"
-    ]
-    
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks"
-    ]
-    
-    @State private var selectedPictrure = Int.random(in: 0...3)
     var body: some View {
-        Image(pictures[selectedPictrure])
-            .resizable()
-            .scaledToFit()
-            .onTapGesture {
-                selectedPictrure = Int.random(in: 0...3)
-            }
-            .accessibilityLabel(labels[selectedPictrure])
-            .accessibilityAddTraits(.isButton)
-            .accessibilityRemoveTraits(.isImage)
+        VStack {
+            Text("your score is")
+            Text("1000")
+        }
+        .accessibilityElement(children: .ignore)
     }
 }
 
