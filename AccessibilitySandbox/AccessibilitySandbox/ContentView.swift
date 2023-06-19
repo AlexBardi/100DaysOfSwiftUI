ae198ca5e90675b62520ceb078df7132c21fc213
Alex Bardi <alex.bardi@hey.com>
2023-06-18 23:18:07 -0500

    Day 74: Doned, learned about different inc/dec UI

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -8,12 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value = 20
+    
     var body: some View {
         VStack {
-            Text("your score is")
-            Text("1000")
+            Text("Value \(value)")
+            
+            Button("Increment") {
+                value += 1
+            }
+            
+            Button("Decrement") {
+                value -= 1
+            }
         }
-        .accessibilityElement(children: .ignore)
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled")
+            }
+        }
     }
 }
 
