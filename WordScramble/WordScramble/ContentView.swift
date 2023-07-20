7ca9fd64a9859992e979fd0ade8961f8ba494e88
Alex Bardi <alex.bardi@hey.com>
2023-07-19 20:13:32 -0500

    Didn't fix guess the flag because I added too many flags, but I fixed word scramble

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -104,6 +104,8 @@ struct ContentView: View {
                                 Image(systemName: "\(word.count).circle")
                                 Text(word)
                             }
+                            .accessibilityElement()
+                            .accessibilityLabel("\(word), \(word.count) letters")
                         }
                     }
                 }
